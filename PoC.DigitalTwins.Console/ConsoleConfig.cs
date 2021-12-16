1452771361aff38a07eae26adbf64b1f63847fd1
Ondrej Štefík <ondrej.stefik@gmail.com>
2021-12-16 20:50:33 +0100

    DigitalTwinsConfig + refactoring

--- PoC.DigitalTwins.Console/ConsoleConfig.cs
+++ PoC.DigitalTwins.Console/ConsoleConfig.cs
@@ -5,19 +5,29 @@
 
     internal class ConsoleConfig
     {
-        private string hostName = String.Empty;
-        private RaspberryConfig raspberryPi = new();
+        [JsonPropertyName("digitalTwins")]
+        public DigitalTwinsConfig DigitalTwins { get; set; } = new();
+
+        [JsonPropertyName("raspberryPi")]
+        public RaspberryConfig RaspberryPi { get; set; } = new();
+    }
+
+    internal class DigitalTwinsConfig
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; } = "";
 
         [JsonPropertyName("hostName")]
-        public string HostName { get => hostName; set => hostName = value; }
-        [JsonPropertyName("raspberryPi")]
-        public RaspberryConfig RaspberryPi { get => raspberryPi; set => raspberryPi = value; }
+        public string HostName { get; set; } = "";
+
+        [JsonPropertyName("resourceGroupName")]
+        public string ResourceGroupName { get; set; } = "";
     }
 
     internal class RaspberryConfig : IRaspberryConfig
     {
         [JsonPropertyName("meteo")]
-        public MeteoConfig? Meteo { get; set; }
+        public MeteoConfig? Meteo { get; set; } = null;
 
         IMeteoConfig? IRaspberryConfig.Meteo => this.Meteo;
     }
